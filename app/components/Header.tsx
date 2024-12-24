25b86dbf571c97daaee9694df53e31da1cad20ca
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2024-12-25 06:50:47 +0900

    仮のheaderを設置

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="w-full bg-red-400 text-white text-xl p-4">
+        <div className="w-full max-w-screen-xl mx-auto flex justify-between items-center gap-4 px-4 md:px-10 lg:px-20">
+          <img
+            className="w-36 h-[50px] sm:w-44 md:w-60 lg:w-72 max-w-full object-cover"
+            src="https://placehold.jp/300x50.png"
+            alt="ロゴ画像"
+          />
+          <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-10 items-center justify-center">
+            <button>Home</button>
+            <button>Create</button>
+            <button>Sign in</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
