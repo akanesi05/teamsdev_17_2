aaf53b110e36abbb844a23cff6343699cbd54112
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2024-12-25 06:50:20 +0900

    tailwind.cssを設定しました

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Header from "./components/Header";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return (
+    <div>
+      <Header />
+      <div className="text-center text-2xl text-blue-500 bg-yellow-200 p-4">Hello, Tailwind CSS!</div>
+    </div>
+  );
 }
