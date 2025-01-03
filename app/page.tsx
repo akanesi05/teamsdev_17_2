0fa8d06e0a50270b133660300c6b183421a734da
荒井美香 <mika8500@yk.commufa.jp>
2025-01-03 17:27:21 +0900

    ホーム画面　検索機能実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,198 +1,70 @@
+'use client'
 import Image from "next/image";
+import { supabase } from "../utils/supabase";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [posts, setPosts] = useState<any>([]);
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const { data } = await supabase.from("posts").select("*");
+    setPosts(data);
+  }
+
+  const search = async (value: string) => {
+    if (value !== "") {
+      const { data: posts, error } = await supabase
+        .from("posts")
+        .select()
+        .textSearch("title", `${value}`);
+      if (error) throw error;
+      setPosts(posts);
+      return;
+    } else {
+      await fetchPosts();
+    }
+  }
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+    search(e.target.value);
+  }
   return (
     <div className="py-16">
       <div className="flex items-center justify-center mb-16">
-        <input type="text" placeholder="Search Blog Post" />
+        <input 
+          type="text"
+          value={keyword}
+          placeholder="Search Blog Post"
+          onChange={(e) => handleChange(e)}
+        />
         <Image src="/images/icon_search.svg" width={41} height={39} alt="検索" />
       </div>
       <div className="my-0 mx-auto w-9/12">
         <div className="flex mb-20">
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
+          {posts.map((post: any) => (
+            <div key={post.id}>
+              <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
+              <div className="flex">
+                <p>{post.title}</p>
+                <p>
+                  <a href="#">Categroy</a>
+                </p>
+              </div>
+              <div className="flex">
+                <p>
+                  <a href="#">Author</a>
+                </p>
+                <p>a min ago</p>
+              </div>
+              <p>{post.content}</p>
             </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-        </div>
-        <div className="flex mb-20">
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-        </div>
-        <div className="flex">
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
-          <div>
-            <Image src="/images/image_picture.png" width={467} height={304.51} alt="仮 画像" />
-            <div className="flex">
-              <p>Post Title</p>
-              <p>
-                <a href="#">Categroy</a>
-              </p>
-            </div>
-            <div className="flex">
-              <p>
-                <a href="#">Author</a>
-              </p>
-              <p>a min ago</p>
-            </div>
-            <p>
-              test test test test test test test test test test test test
-              <br />
-              test test test test test test test test test test test test
-            </p>
-          </div>
+          ))}
         </div>
       </div>
       <div className="flex items-center justify-center mt-16">
