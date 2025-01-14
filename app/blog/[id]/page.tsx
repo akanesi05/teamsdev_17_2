b175a4a860850973165ba1362fcf48e73500bebf
mikArai5 <74860483+mikArai5@users.noreply.github.com>
2025-01-14 21:57:38 +0900

    Comments取得&投稿処理実装 (#57)
    * ホーム画面　検索機能実装
    
    * ホーム画面　検索機能　微調整
    
    * ホーム画面　検索機能修正
    
    * ホーム画面　検索機能　PR前チェック
    
    * ホーム画面　検索機能　プルリクコメント箇所修正
    
    * ブログ　コメント投稿機能　作業前
    
    * ブログ　コメント投稿機能　作業前
    
    * ホーム画面　検索機能　プルリクコメント箇所修正
    
    * ブログビュー画面　詳細記事表示
    
    * ブログ詳細ページ　対象記事表示
    
    * コメント　投稿機能　追加
    
    * コメント追加機能　修正
    
    * コメント追加機能　修正
    
    * コメント追加機能　修正
    
    * コメント追加機能　プルリク　コメント箇所修正
    
    * コメント追加機能　lint/ format チェック
    
    * コメント追加機能　不要タグ削除

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,29 +1,69 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Pagenation } from "../../components/Pagenation";
 import { RoundButton } from "@/app/components/RoundButton";
+import { supabase } from "../../../utils/supabase";
 
-export default function BlogPage() {
+type PageProps = {
+  params: {
+    id: string;
+    title: string;
+    content: string;
+    image_path: string;
+    user_id: string;
+  };
+};
+
+type Comments = {
+  id: string;
+  content: string;
+};
+
+const BlogPage = ({ params }: PageProps) => {
+  const [comment, setComment] = useState<string>("");
+  const [comments, setComments] = useState<Comments[]>([]);
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  async function fetchComments() {
+    const { data } = await supabase
+      .from("comments")
+      .select("*")
+      .eq("post_id", params.id)
+      .order("created_at", { ascending: false });
+    setComments(data);
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (comment === "") return;
+
+    const { data } = await supabase.from("comments").select("*").eq("post_id", params.id);
+
+    // TODO [サインイン機能追加後user_idの取得方法を変更する必要があります]
+    await supabase.from("comments").insert({
+      content: comment,
+      post_id: params.id,
+      user_id: data[0].user_id,
+    });
+    setComment("");
+  };
+
   return (
     <>
       <div className="l-wrapper">
         <div className="l-inner mx-4 my-6 sm:w-10/12 sm:max-w-7xl sm:mx-auto sm:my-14">
-          {/* blog detail card */}
           <div className="p-blogCard w-full rounded-3xl mb-12 sm:py-12 sm:px-16 sm:bg-[#D9D9D991]">
             <div className="p-blogCard-head flex justify-between items-center mb-8">
               <h1 className="p-blogCard__title text-4xl sm:text-5xl">Blog Title</h1>
               <span className="p-blogCard__icon">
-                <Image
-                  src="/images/icon-user.png" //配置した画像のパスを記述する。
-                  alt="blog Image"
-                  width={96}
-                  height={94}
-                  className="w-14 sm:w-24"
-                />
+                <Image src="/images/icon-user.png" alt="blog Image" width={96} height={94} className="w-14 sm:w-24" />
               </span>
             </div>
             <div className="p-blogCard__img mb-8 sm:mb-12">
-              {/* <img src="/_next/static/img/blog/detail/sample-image.png" alt="sample image" /> */}
               <Image
                 src="/images/blog-sample1.png" //配置した画像のパスを記述する。
                 alt="blog Image"
@@ -49,11 +89,11 @@ export default function BlogPage() {
               <li>
                 <a href="">
                   <Image
-                    src="/images/blog-sample2.png"
-                    alt="blog sample2"
-                    width={333}
-                    height={166}
-                    className="w-full mb-2"
+                    src="/images/blog-sample1.png" //配置した画像のパスを記述する。
+                    alt="blog Image"
+                    width={2395}
+                    height={1237}
+                    className="w-full"
                   />
                   <p className="leading-9 font-bold text-lg">Post Title</p>
                 </a>
@@ -87,53 +127,43 @@ export default function BlogPage() {
           {/* comments */}
           <div className="p-comments max-w-[776px] m-auto">
             <h2 className="InputArea__title text-3xl mb-7">Comments</h2>
-            <div className="p-comments__InputArea InputArea grid grid-cols-[auto,100px] gap-6 sm:gap-8 sm:grid-cols-[auto,172px] mb-7">
+            <form
+              className="p-comments__InputArea InputArea grid grid-cols-[auto,100px] gap-6 sm:gap-8 sm:grid-cols-[auto,172px] mb-7"
+              onSubmit={(e) => handleSubmit(e)}
+            >
               <input
+                id="comment"
                 className="rounded-md border border-solid border-[#000] text-center p-2"
                 type="text"
+                value={comment}
                 placeholder="your comments..."
+                onChange={(e) => setComment(e.target.value)}
               />
               <button className="InputArea__button w-full text-white rounded-md bg-[#18A0FB]">Comment</button>
-            </div>
+            </form>
             <ul className="p-comments__post">
-              <li className="c-userComment bg-[#C4C4C44D] rounded-md p-5 grid gap-7 grid-cols-[64px,auto] mb-7">
-                <div className="c-userComment__head text-center">
-                  <Image
-                    src="/images/icon-user.png" //配置した画像のパスを記述する。
-                    alt="blog Image"
-                    width={64}
-                    height={64}
-                    className="mb-2"
-                  />
-                  <p>USER</p>
-                </div>
-                <div className="c-userComment__body">
-                  <p className="text-lg mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula nibh, interdum non enim sit
-                    amet, iaculis aliquet nunc.
-                  </p>
-                  <p className="text-[#18A0FB80] text-lg">a min ago</p>
-                </div>
-              </li>
-              <li className="c-userComment bg-[#C4C4C44D] rounded-md p-5 grid gap-7 grid-cols-[64px,auto] mb-7">
-                <div className="c-userComment__head text-center">
-                  <Image
-                    src="/images/icon-user.png" //配置した画像のパスを記述する。
-                    alt="blog Image"
-                    width={64}
-                    height={64}
-                    className="mb-2"
-                  />
-                  <p>USER</p>
-                </div>
-                <div className="c-userComment__body">
-                  <p className="text-lg mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula nibh, interdum non enim sit
-                    amet, iaculis aliquet nunc.
-                  </p>
-                  <p className="text-[#18A0FB80] text-lg">a min ago</p>
-                </div>
-              </li>
+              {comments.map((comment) => (
+
+                  <li
+                    key={comment.id}
+                    className="c-userComment bg-[#C4C4C44D] rounded-md p-5 grid gap-7 grid-cols-[64px,auto] mb-7"
+                  >
+                    <div className="c-userComment__head text-center">
+                      <Image
+                        src="/images/icon-user.png" //配置した画像のパスを記述する。
+                        alt="blog Image"
+                        width={64}
+                        height={64}
+                        className="mb-2"
+                      />
+                      <p>USER</p>
+                    </div>
+                    <div className="c-userComment__body">
+                      <p className="text-lg mb-4">{comment.content}</p>
+                      <p className="text-[#18A0FB80] text-lg">a min ago</p>
+                    </div>
+                  </li>
+              ))}
             </ul>
           </div>
           {/* button components */}
@@ -146,4 +176,5 @@ export default function BlogPage() {
       </div>
     </>
   );
-}
+};
+export default BlogPage;
