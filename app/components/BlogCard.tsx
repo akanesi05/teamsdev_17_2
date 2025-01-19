d7f66b8338f8aec3feb4563254448a4151d6d279
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-19 20:38:10 +0900

    途中

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -29,37 +29,18 @@ export default function BlogCard ()  {
             <div>
               <h2 className="text-3xl font-semibold mb-2"></h2>
               <div className="flex gap-5">
+                <p>{post.title}</p>
                 <p className="text-gray-500">Author</p>
+        
                 <p className="text-gray-500">1日前</p>
               </div>
             </div>
             <p className="text-lg">Category</p>
           </div>
           <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
-          
+          <Link href={`/blog/${post.id}`}>詳細</Link>
         </div>
       </a>))}
-      <a
-        href="#"
-        className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
-      >
-        <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
-        <div className="px-6 py-4">
-          
-          <div className="flex justify-between mb-2">
-            <div>
-              <h2 className="text-3xl font-semibold mb-2"></h2>
-              <div className="flex gap-5">
-                <p className="text-gray-500">Author</p>
-                <p className="text-gray-500">1日前</p>
-              </div>
-            </div>
-            <p className="text-lg">Category</p>
-          </div>
-          <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
-          
-        </div>
-      </a>
       {/* END カード */}
        <div className="flex mb-20">
       
