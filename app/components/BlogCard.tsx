9988a8b459abce3d1fe81a43207fd29afc9d0f5a
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-19 20:08:10 +0900

    途中

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -1,18 +1,33 @@
+"use client"
 import React from "react";
+import { supabase } from "@/utils/supabase";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+export default function BlogCard ()  {
+  const [posts, setPosts] = useState<any>([]);
 
-const BlogCard = () => {
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const { data } = await supabase.from("posts").select("*");
+    setPosts(data);
+  }
+
   return (
     <div className="max-w-[1580px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-24 px-4">
       {/* START カード */}
-      <a
+      {posts.map((post: any) => ( <a
         href="#"
         className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
       >
         <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
         <div className="px-6 py-4">
+          
           <div className="flex justify-between mb-2">
             <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
+              <h2 className="text-3xl font-semibold mb-2"></h2>
               <div className="flex gap-5">
                 <p className="text-gray-500">Author</p>
                 <p className="text-gray-500">1日前</p>
@@ -21,19 +36,19 @@ const BlogCard = () => {
             <p className="text-lg">Category</p>
           </div>
           <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+          
         </div>
-      </a>
-      {/* END カード */}
-      {/* START カード */}
+      </a>))}
       <a
         href="#"
         className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
       >
         <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
         <div className="px-6 py-4">
+          
           <div className="flex justify-between mb-2">
             <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
+              <h2 className="text-3xl font-semibold mb-2"></h2>
               <div className="flex gap-5">
                 <p className="text-gray-500">Author</p>
                 <p className="text-gray-500">1日前</p>
@@ -42,95 +57,31 @@ const BlogCard = () => {
             <p className="text-lg">Category</p>
           </div>
           <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+          
         </div>
       </a>
       {/* END カード */}
-      {/* START カード */}
-      <a
-        href="#"
-        className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
-      >
-        <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
-        <div className="px-6 py-4">
-          <div className="flex justify-between mb-2">
-            <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
-              <div className="flex gap-5">
-                <p className="text-gray-500">Author</p>
-                <p className="text-gray-500">1日前</p>
+       <div className="flex mb-20">
+      
+          {posts.map((post: any) => (
+            <div key={post.id}>
+              <div className="flex">
+                <p>{post.title}</p>
+                <p>
+                  <a href="#">Categroy</a>
+                </p>
               </div>
-            </div>
-            <p className="text-lg">Category</p>
-          </div>
-          <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
-        </div>
-      </a>
-      {/* END カード */}
-      {/* START カード */}
-      <a
-        href="#"
-        className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
-      >
-        <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
-        <div className="px-6 py-4">
-          <div className="flex justify-between mb-2">
-            <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
-              <div className="flex gap-5">
-                <p className="text-gray-500">Author</p>
-                <p className="text-gray-500">1日前</p>
+              <div className="flex">
+               
               </div>
+              <p>{post.content}</p>
+              <Link href={`/blog/${post.id}`}>詳細</Link>
             </div>
-            <p className="text-lg">Category</p>
-          </div>
-          <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+          ))}
         </div>
-      </a>
-      {/* END カード */}
-      {/* START カード */}
-      <a
-        href="#"
-        className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
-      >
-        <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
-        <div className="px-6 py-4">
-          <div className="flex justify-between mb-2">
-            <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
-              <div className="flex gap-5">
-                <p className="text-gray-500">Author</p>
-                <p className="text-gray-500">1日前</p>
-              </div>
-            </div>
-            <p className="text-lg">Category</p>
-          </div>
-          <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
-        </div>
-      </a>
-      {/* END カード */}
-      {/* START カード */}
-      <a
-        href="#"
-        className="block max-w-[467px] bg-white rounded-lg shadow-md overflow-hidden duration-300 hover:scale-105 hover:shadow-lg"
-      >
-        <img src="https://via.placeholder.com/467x300" alt="" className="w-full object-cover" />
-        <div className="px-6 py-4">
-          <div className="flex justify-between mb-2">
-            <div>
-              <h2 className="text-3xl font-semibold mb-2">タイトル</h2>
-              <div className="flex gap-5">
-                <p className="text-gray-500">Author</p>
-                <p className="text-gray-500">1日前</p>
-              </div>
-            </div>
-            <p className="text-lg">Category</p>
-          </div>
-          <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
-        </div>
-      </a>
-      {/* END カード */}
-    </div>
+    </div> 
+    
   );
 };
 
-export default BlogCard;
+
