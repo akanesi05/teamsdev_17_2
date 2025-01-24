7818c1fde66c8871cfe21a8f506729ecbab1acd9
rain2124 <bosu1305@gmail.com>
2025-01-24 22:32:51 +0900

    userid取得箇所調整

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -23,8 +23,8 @@ const CreatePage = () => {
     const {data} = await supabase.auth.getUser();
     await supabase.from("posts").insert({
       id: uuidv4(),
-      // user_id: "0e4d4176-0660-4e5f-8ae1-48716ac124b5",
-      user_id: data.user?.id,//
+      user_id: "0e4d4176-0660-4e5f-8ae1-48716ac124b5",
+      // user_id: data.user?.id,
       category_id: "c846a032-b069-4981-8e02-328fd7f9b9d5", // ここはこのままで良い（カテゴリ登録機能がないため）
       title: blogTitle,
       content: blogContent,
