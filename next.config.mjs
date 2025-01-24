64e40b4d080725cd49e5483e624f07706320589a
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2025-01-25 08:09:50 +0900

    remotePatternsに変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cvrottcjnnudnbfhwkcj.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname
+      }
+    ],
   },
 };
 
