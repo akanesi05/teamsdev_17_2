fa88dc684726f8daf32d932e49b8fc7a6c2ee203
浅野 弘也 <43087605+rain2124@users.noreply.github.com>
2025-01-26 09:29:59 +0900

    Feature/page create (#64)
    * create 途中
    
    * posts insertが成功するコードを追加。
    
    * #50 記事投稿システム実装
    
    * userid取得箇所調整
    
    * main merge
    
    * userid連携
    
    * npm run format対応
    
    ---------
    
    Co-authored-by: s.matsubara <shogyomujo11@gmail.com>

--- app/create/components/create/ImageUpload.tsx
+++ app/create/components/create/ImageUpload.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-const ImageUpload = () => {
+const ImageUpload = (props: any) => {
   return (
     <div className="w-full h-[300px] border-dotted border-2 border-black rounded-3xl p-6 flex justify-center items-center mt-20">
       <label
@@ -8,10 +6,9 @@ const ImageUpload = () => {
         className="flex items-center justify-center bg-[#18A0FB] text-white px-4 rounded-full cursor-pointer w-96 h-28 max-h-28 max-w-full text-[20px] sm:text-[33px] hover:bg-blue-300 transition duration-200"
       >
         Upload Image
-        <input id="image-upload" type="file" accept="image/*" className="hidden" />
+        <input id="image-upload" type="file" accept="image/*" className="hidden" onChange={props.onChange} />
       </label>
     </div>
   );
 };
-
 export default ImageUpload;
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,18 +1,56 @@
-import React from "react";
-import TextArea from "../components/TextArea";
-import TitleInput from "../components/TitleInput";
-import ImageUpload from "../components/ImageUpload";
-import CreateButton from "../components/CreateButton";
+"use client";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { supabase } from "../../utils/supabase";
+import TitleInput from "./components/create/TitleInput";
+import { uploadStorage } from "./components/create/storage";
+import ImageUpload from "./components/create/ImageUpload";
+import TextArea from "./components/create/TextArea";
+import CreateButton from "./components/create/CreateButton";
 
 const CreatePage = () => {
+  const [blogTitle, setBlogTitle] = useState("");
+  const [blogContent, setBlogContent] = useState("");
+  const [blogImage, setBlogImage] = useState<File | null>(null);
+  const handleInsertTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBlogTitle(e.target.value);
+  };
+  const handleInsertContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBlogContent(e.target.value);
+  };
+  const handleInsertImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBlogImage(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const imageUrl = await uploadStorage(blogImage);
+    const authUser = await supabase.auth.getUser();
+    const userID = authUser.data.user?.id;
+    if (!userID) return;
+    if (blogTitle === "") return;
+    await supabase.from("posts").insert({
+      id: uuidv4(),
+      user_id: userID,
+      category_id: "c846a032-b069-4981-8e02-328fd7f9b9d5", // ここはこのままで良い（カテゴリ登録機能がないため）
+      title: blogTitle,
+      content: blogContent,
+      image_path: imageUrl,
+    });
+    setBlogTitle("");
+    setBlogContent("");
+    setBlogImage(null);
+  };
   return (
     <div className="container mx-auto px-4 pb-32">
       <h1 className="text-5xl font-bold text-center my-20">Create Blog </h1>
       <div className="max-w-[1580px] mx-auto px-4"></div>
-      <TitleInput />
-      <ImageUpload />
-      <TextArea />
-      <CreateButton />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <TitleInput value={blogTitle} onChange={handleInsertTitle} />
+        <ImageUpload onChange={handleInsertImage} />
+        <TextArea value={blogContent} onChange={handleInsertContent} />
+        <CreateButton />
+      </form>
     </div>
   );
 };
